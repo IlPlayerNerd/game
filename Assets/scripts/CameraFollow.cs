4fafd1fd7e7fbc94c6ad8dab0d7b3faac31dbc75
WhyIsEveryUsernameIsAlreadyTaken <51572004+WhyIsEveryUsernameIsAlreadyTaken@users.noreply.github.com>
2021-12-12 14:09:13 -0800

    Imported Portal 2 Textures (nomo

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;
-    public Vector3 offset = Vector3.zero;
-    public float distance = 5;
+    //public GameObject player;
+    //public Vector3 offset = Vector3.zero;
+    //public float distance = 5;
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = player.transform.position + offset - (player.transform.forward * distance);
-        transform.LookAt(player.transform, Vector3.up);
-    }
+    //void Update()
+    //{
+    //  transform.position = player.transform.position + offset - (player.transform.forward * distance);
+    //  transform.LookAt(player.transform, Vector3.up);
+    //}
 }
--- Assets/scripts/movementIlPlayerNErd.cs
+++ Assets/scripts/movementIlPlayerNErd.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class movementIlPlayerNErd : MonoBehaviour
 {
-    Rigidbody rb;
-    Vector3 moveTo = Vector3.zero;
+    //Rigidbody rb;
+    //Vector3 moveTo = Vector3.zero;
 
-    public float speed = 10f;
+    //public float speed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //getting the game obj's rb
-        rb = GetComponent<Rigidbody>();
-        rb.useGravity = false;
-    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    //getting the game obj's rb
+    //    rb = GetComponent<Rigidbody>();
+    //    rb.useGravity = false;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //to get input form  unity's input system
-        float z = Input.GetAxisRaw("Horizontal");
-        float x = Input.GetAxisRaw("Vertical");
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //to get input form  unity's input system
+    //    float z = Input.GetAxisRaw("Horizontal");
+    //    float x = Input.GetAxisRaw("Vertical");
 
-        moveTo = transform.forward * x + transform.right * z;
-    }
+    //    moveTo = transform.forward * x + transform.right * z;
+    //}
 
-    private void FixedUpdate()
-    {
-        //moving the player with rb, and also making it frame independant by mulktiplying by Time.deltaTime
-        rb.MovePosition(transform.position + moveTo * speed * Time.deltaTime);
-    }
+    //private void FixedUpdate()
+    //{
+    //    //moving the player with rb, and also making it frame independant by mulktiplying by Time.deltaTime
+    //    rb.MovePosition(transform.position + moveTo * speed * Time.deltaTime);
+    //}
 }
